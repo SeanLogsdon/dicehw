0fe2d9e083526951a75915266bf41916ee096616
SeanLogsdon <seanlogsdon14@gmail.com>
2020-09-15 01:12:37 -0500

    Added splash screen and cases to d20.

--- app/src/main/java/com/example/dicehw/MainActivity.java
+++ app/src/main/java/com/example/dicehw/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import java.util.Random;
 
+
 public class MainActivity extends AppCompatActivity {
     private ImageView imageViewDice;
     private Random rng = new Random();
